4fa5700bc1ff752f43a2ba9293ed042d1391b272
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2021-08-01 23:49:37 +0530

    room frontend

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,51 @@ const socket = io();
 
 // Elements
 
-const $nameForm = document.querySelector(".name-form");
+const $nameForm = $(".name-form");
+const $joinRoomBtn = $(".join-btn");
+const $createRoomBtn = $(".create-btn");
+const $cancelBtn = $(".cancel");
+const $joinRoomBuffer = $(".join-room-buffer");
+const $createRoomBuffer = $(".create-room-buffer");
+
+// VARIABLES
+
+let username = "Player";
 
 // Functions
 
-$nameForm.addEventListener("submit", (e) => {
+$nameForm.on("submit", (e) => {
   e.preventDefault();
 
-  const username = document.querySelector("#username").value;
-  document.querySelector(".name-section").classList.add("left");
-  document.querySelector(".dashboard").classList.remove("right");
+  username = $("#username").value;
+  $(".name-section").addClass("left");
+  $(".rooms").removeClass("right");
+});
+
+$joinRoomBtn.on("click", (e) => {
+  e.preventDefault();
+
+  $joinRoomBtn.hide();
+  $createRoomBuffer.addClass("hide");
+  $createRoomBtn.hide();
+
+  $joinRoomBuffer.removeClass("hide");
+});
+
+$createRoomBtn.on("click", (e) => {
+  e.preventDefault();
+
+  $joinRoomBtn.hide();
+  $joinRoomBuffer.addClass("hide");
+  $createRoomBtn.hide();
+
+  $createRoomBuffer.removeClass("hide");
+});
+
+$cancelBtn.on("click", function (e) {
+  e.preventDefault();
+  $(this).parent().parent().addClass("hide");
+
+  $joinRoomBtn.show();
+  $createRoomBtn.show();
 });
